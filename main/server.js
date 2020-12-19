76184e33f058944a91805419e83ae26384abbc2f
amanduke <65379991+amanduke@users.noreply.github.com>
2020-12-19 14:49:56 -0500

    server.js update

--- main/server.js
+++ main/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const db = require('./config/connection');
+const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
 
-app.listen(PORT, () => console.log(`Connected on http://localhost:${PORT}`));
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
