cd827e9037d4e1058510d3bbb9eeab122ba0732b
amanduke <65379991+amanduke@users.noreply.github.com>
2020-12-19 14:24:32 -0500

    user-routes

--- main/routes/api/index.js
+++ main/routes/api/index.js
@@ -1,10 +1,9 @@
+
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 
-router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-  return res.send('Wrong route!');
-});
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
